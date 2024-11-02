fb70c51034d4e6151d68c0ffd49dd071e11b41fa
Dolphinator7 <Dolphinator007@gmail.com>
2024-11-02 18:47:09 +0100

    all app

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': "1f77276b2fmsha78721a86ca7e98p103720jsn52bf753c47d9",
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': '1f77276b2fmsha78721a86ca7e98p103720jsn52bf753c47d9',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
